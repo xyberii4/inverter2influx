e4ba798855e09160d39a91da4bf0ed883bf82736
xyberii4 <61101529+xyberii4@users.noreply.github.com>
2024-08-26 14:23:12 +0800

    error handling

--- config/start_wifi.sh
+++ config/start_wifi.sh
@@ -17,28 +17,53 @@ dhcpcd="/etc/dhcpcd.conf"
 dnsmasq="/etc/dnsmasq.conf"
 
 
-#rewrite network files
-sudo cat $wps_wifi > $wps
-echo "Updated wpa_supplicant.conf"
-sleep 2
-sudo cat $dhcpcd_wifi > $dhcpcd
-echo "Updated dhcpcd.conf"
-sleep 2
-sudo cat $dnsmasq_wifi > $dnsmasq
-echo "Updated dnsmasp.conf"
-sleep 2
+#check if wpa_wifi has been edited
+wpa_wifi_count=wc -m < $wpa_wifi
+if [[ $wpa_wifi_count -ne 188 ]] ; then
+    #rewrite network files
+    sudo cat $wps_wifi > $wps
+    echo "Updated wpa_supplicant.conf"
+    sleep 2
+    sudo cat $dhcpcd_wifi > $dhcpcd
+    echo "Updated dhcpcd.conf"
+    sleep 2
+    sudo cat $dnsmasq_wifi > $dnsmasq
+    echo "Updated dnsmasp.conf"
+    sleep 2
 
-echo "Disabling nginx..."
-sudo systemctl disable nginx
-sleep 2
+    echo "Disabling nginx..."
+    sudo systemctl disable nginx
+    sleep 2
 
-echo "Restarting network services..."
-sleep 2
-echo "  Refreshing wpa..."
-wpa_cli -i wlan0 reconfigure
-sleep 2
-echo "  Restarting dhcpcd and dnsmasq..."
-systemctl restart dhcpcd dnsmasq
-sleep 2
+    echo "Restarting network services..."
+    sleep 2
+    echo "  Refreshing wpa..."
+    wpa_cli -i wlan0 reconfigure
+    sleep 2
+    echo "  Restarting dhcpcd and dnsmasq..."
+    systemctl restart dhcpcd dnsmasq
+    sleep 2
 
-echo "Pi successfully starting in client mode"
+    echo "Pi successfully starting in client mode"
+
+    #check if Wi-Fi works, otherwise switch back to AP mode
+    ((count = 30))
+    while [[ $count -ne 0 ]] ; do
+        ping -c 1 www.google.com
+        rc=$?
+        if [[ $rc -ne 0 ]] ; then
+            ((count = count - 1))
+        else
+            sleep 0.5
+            ((count = 30))
+        fi
+    done
+
+    if [[ $rc -ne 0 ]] ; then
+        echo "Invalid Wi-Fi connection. Switching to AP mode..."
+        /home/admin/config/start_ap.sh
+    fi
+else
+    echo "Wi-Fi network not added. Switching to AP mode..."
+    /home/admin/config/start_ap.sh
+fi
